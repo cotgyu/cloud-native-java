2d58dca2ecfeeb1e07eabd3a29e7054a804d23b2
cotgyu <v123v123s@naver.com>
2020-12-09 23:40:06 +0900

    chap 2 - 애플리케이션 생성, 테스트

--- src/main/java/com/example/cloudnativejava/CloudNativeJavaApplication.java
+++ src/main/java/com/example/cloudnativejava/CloudNativeJavaApplication.java
@@ -2,6 +2,8 @@ package com.example.cloudnativejava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,3 +17,36 @@ public class CloudNativeJavaApplication {
 	}
 
 }
+
+@Entity
+class Cat{
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+
+	Cat() {}
+
+	public Cat(String name){
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+
+	public Long getId(){
+		return id;
+	}
+
+	public String getName(){
+		return name;
+	}
+}
+
+@RepositoryRestResource
+interface CatRepository extends JpaRepository<Cat, Long>{}
--- src/test/java/com/example/cloudnativejava/CloudNativeJavaApplicationTests.java
+++ src/test/java/com/example/cloudnativejava/CloudNativeJavaApplicationTests.java
@@ -1,13 +1,59 @@
 package com.example.cloudnativejava;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class CloudNativeJavaApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private CatRepository catRepository;
+
+
+	public void before() throws Exception{
+		Stream.of("Felix", "Garfield", "Whiskers")
+				.forEach(n -> catRepository.save(new Cat(n)));
+	}
+
+
+	@Test
+	public void catsReflectedInRead() throws Exception{
+		// charset 설정하는 곳이 없음.. 일단 주석
+		//MediaType halJson = MediaType.parseMediaType("application/hal+json;charset=UTF-8");
+		MediaType halJson = MediaType.parseMediaType("application/hal+json");
+
+
+		this.mockMvc
+				.perform(get("/cats"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(halJson))
+				.andExpect(
+						mvcResult -> {
+							String contentAsString = mvcResult.getResponse().getContentAsString();
+
+							assertThat(contentAsString.split("totalElements")[1].split(":")[1].trim().split(",")[0].equals("3"));
+						}
+				);
+	}
 }
